d5219ad6f26c97c5217b2d8f912473fc591e3d3e
sasa42 <keeponshading@gmail.com>
2020-12-24 21:25:00 +0100

    add start .bat´s for reference resolutions and quality settings

---
+++ 4_start_VHair_F0_PW_2560x780_M1_SQ_High_WM_EX_SI_DX11.bat
@@ -0,0 +1,5 @@
+
+
+start /D .\VHair VHair.exe -screen-fullscreen 0 -popupwindow -screen-width 2560 -screen-height 780 -monitor 1 -screen-quality High -window-mode exclusive -single-instance -force-d3d11
+
+
---
+++ 4_start_VHair_F0_PW_3840x2160_M1_SQ_High_WM_EX_SI_DX11.bat
@@ -0,0 +1,5 @@
+
+
+start /D .\VHair VHair.exe -screen-fullscreen 0 -popupwindow -screen-width 3840 -screen-height 2160 -monitor 1 -screen-quality High -window-mode exclusive -single-instance -force-d3d11
+
+
